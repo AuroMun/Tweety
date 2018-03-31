0967389450861da4b868fd03e0b93e4dec133303
Shruti Gupta <shruti1337@gmail.com>
2018-03-31 13:05:17 +0530

    Re

--- models/menu.py
+++ models/menu.py
@@ -5,11 +5,29 @@
 # this is the main application menu add/remove items as required
 # ----------------------------------------------------------------------------------------------------------------------
 
+# response.title = "Tweety"
+# response.subtitle = "Cheep cheep cheep cheep"
+# response.menu = [
+#     (T('Home'), False, URL('default','home')),
+#     (T('Profile'), False, URL('default','profile')),
+#     (T('Search'), False, URL('default','search')),
+#     (T('Notifs'), False, URL('default','notifs')),
+#     ]
+def item(name):
+    if auth.is_logged_in():
+        notifCount = db((db.notifs.person==auth.user.id) & (db.notifs.opened==False)).count()
+        if notifCount > 0:
+            return A(name+ " " + str(notifCount), _href=URL('default', 'notifs'), _style="color:red;")
+        else:
+            return A(name, _href=URL('default', 'notifs'), _style="color:grey;")
+    else:
+        return A(name, _href=URL('default', 'notifs'), _style="color:grey;")
+
 response.title = "Tweety"
 response.subtitle = "Cheep cheep cheep cheep"
 response.menu = [
-    (T('Home'), False, URL('default','home')),
-    (T('Profile'), False, URL('default','profile')),
-    (T('Search'), False, URL('default','search')),
-    (T('Notifs'), False, URL('default','notifs')),
+    (A('Home', _href=URL('default', 'home'), _style="padding-left:10px;padding-right:20px;color:grey"), False, None),
+    (A('Search', _href=URL('default', 'search'), _style="padding-left:10px;padding-right:20px;color:grey"), False, None),
+    (A('Profile', _href=URL('default', 'profile'), _style="padding-left:10px;padding-right:20px;color:grey"), False, None),
+    (item('Notifs'), False, None),
     ]
